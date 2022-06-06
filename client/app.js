7d97a536a6638b94393361bed175cbf8f2a72a38
Luis Alberto Jimenez Soto <lajs5257@gmail.com>
2022-06-06 11:21:54 -0500

    ya se loguea con metamask

--- client/app.js
+++ client/app.js
@@ -29,76 +29,18 @@ App = {
     try {
       const res = await fetch("RecordsContract.json");
       const recordsContractJSON = await res.json();
-      App.contracts.RecordsContract = TruffleContract(recordsContractJSON);
+      App.contracts.RecordsContract =
+        TruffleContract(recordsContractJSON);
       App.contracts.RecordsContract.setProvider(App.web3Provider);
 
-      App.recordsContract = await App.contracts.RecordsContract.deployed();
+      App.recordsContract =
+        await App.contracts.RecordsContract.deployed();
     } catch (error) {
       console.error(error);
     }
   },
   render: async () => {
-    document.getElementById("account").innerText = App.account;
+    //document.getElementById("account").innerText = App.account;
     console.log("Account: " + App.account);
-  },
-  renderTasks: async () => {
-    const tasksCounter = await App.recordsContract.tasksCounter();
-    const taskCounterNumber = tasksCounter.toNumber();
-
-    let html = "";
-
-    for (let i = 1; i <= taskCounterNumber; i++) {
-      const task = await App.recordsContract.tasks(i);
-      const taskId = task[0].toNumber();
-      const taskTitle = task[1];
-      const taskDescription = task[2];
-      const taskDone = task[3];
-      const taskCreatedAt = task[5];
-      const taskCreatedBy = task[4];
-      console.log("Account actual: " + App.account);
-      console.log("Account recuperada: " + taskCreatedBy);
-      if (taskCreatedBy === App.account) {
-        // Creating a task Card
-        let taskElement = `<div class="card bg-dark rounded-0 mb-2">
-        <div class="card-header d-flex justify-content-between align-items-center">
-          <span>${taskTitle}</span>
-          <div class="form-check form-switch">
-            <input class="form-check-input" data-id="${taskId}" type="checkbox" onchange="App.toggleDone(this)" ${
-          taskDone === true && "checked"
-        }>
-          </div>
-        </div>
-        <div class="card-body">
-          <span>${taskDescription}</span>
-          <span>${taskDone}</span>
-          <p class="text-muted">Task was created ${new Date(
-            taskCreatedAt * 1000
-          ).toLocaleString()}</p>
-          </label>
-        </div>
-      </div>`;
-        html += taskElement;
-      }
-    }
-
-    document.querySelector("#tasksList").innerHTML = html;
-  },
-  createTask: async (title, description) => {
-    try {
-      const result = await App.recordsContract.createTask(title, description, App.account, {
-        from: App.account,
-      });
-      console.log(result.logs[0].args);
-      window.location.reload();
-    } catch (error) {
-      console.error(error);
-    }
-  },
-  toggleDone: async (element) => {
-    const taskId = element.dataset.id;
-    await App.recordsContract.toggleDone(taskId, {
-      from: App.account,
-    });
-    window.location.reload();
   },
 };
--- client/src/views/login.vue
+++ client/src/views/login.vue
@@ -1,117 +1,174 @@
 <template>
-<v-container>
-   <img id="logo" class="itemheader" :src="require('@/assets/img/logo.png')">
-   <div class=topright>Dont have an account?
-      <v-btn rounded outlined><b>Sign Up</b>
-      </v-btn>
-   </div>
-   <v-layout >
+  <v-container>
+    <img id="logo" class="itemheader" :src="require('@/assets/img/logo.png')" />
+    <div class="topright">
+      Dont have an account?
+      <v-btn rounded outlined><b>Sign Up</b> </v-btn>
+    </div>
+    <v-layout>
       <v-flex>
-         <div class=centered>
-            <h1>Welcome</h1>
-            <v-text-field
-               type=""
-               label="Email"
-               rounded
-               single-line
-               outlined></v-text-field>
-            <v-text-field
-               type="password"
-               label="Password"
-               rounded
-               single-line
-               outlined
-               class="shrink">
-            </v-text-field>
-               <v-col align="center" justify="space-around">
-                  <v-btn id="logbtn" rounded color="#fff176"><b>Login</b></v-btn>
-               </v-col>
-               <div class="test">
-                  <hr class="solid">
-                  Login with
-               </div>
-               <div>
-                  <v-col align="center">
-                     <div>
-                        <v-btn id="SocialMediabtn"
-                        rounded
-                        outlined
-                     >
-                        <img src="@/assets/img/metamask.png">
-                        Login with Metamask
-                        </v-btn>                  
-                     </div>
-                  </v-col>
-               </div>
-         </div>
+        <div class="centered">
+          <h1>Welcome</h1>
+          <v-text-field
+            type=""
+            label="Email"
+            rounded
+            single-line
+            outlined
+          ></v-text-field>
+          <v-text-field
+            type="password"
+            label="Password"
+            rounded
+            single-line
+            outlined
+            class="shrink"
+          >
+          </v-text-field>
+          <v-col align="center" justify="space-around">
+            <v-btn id="logbtn" rounded color="#fff176"><b>Login</b></v-btn>
+          </v-col>
+          <div class="test">
+            <hr class="solid" />
+            Login with
+          </div>
+          <div>
+            <v-col align="center">
+              <div>
+                <v-btn id="SocialMediabtn" rounded outlined @click="onClick">
+                  <img src="@/assets/img/metamask.png" />
+                  Login with Metamask
+                </v-btn>
+              </div>
+            </v-col>
+          </div>
+        </div>
       </v-flex>
-   </v-layout>
-</v-container>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-}
+  name: "Example",
+  data() {
+    return {
+      account: null,
+      userType: null,
+    };
+  },
+  methods: {
+    onClick() {
+      var App = {
+        contracts: {},
+        init: async () => {
+          await App.loadWeb3();
+          await App.loadAccount();
+          await App.loadContract();
+          //await App.render();
+          //await App.renderTasks();
+        },
+        loadWeb3: async () => {
+          if (window.ethereum) {
+            App.web3Provider = window.ethereum;
+            await window.ethereum.request({ method: "eth_requestAccounts" });
+          } else if (web3) {
+            web3 = new Web3(window.web3.currentProvider);
+          } else {
+            console.log(
+              "No ethereum browser is installed. Try it installing MetaMask "
+            );
+          }
+        },
+        loadAccount: async () => {
+          const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+          });
+          App.account = accounts[0];
+          console.log(App.account);
+        },
+        loadContract: async () => {
+          try {
+            const res = await fetch("RecordsContract.json");
+            const recordsContractJSON = await res.json();
+            App.contracts.RecordsContract =
+              TruffleContract(recordsContractJSON);
+            App.contracts.RecordsContract.setProvider(App.web3Provider);
+
+            App.recordsContract =
+              await App.contracts.RecordsContract.deployed();
+          } catch (error) {
+            console.error(error);
+          }
+        },
+        render: async () => {
+          //document.getElementById("account").innerText = App.account;
+          console.log("Account: " + App.account);
+        },
+      };
+      console.log('hola perrras');
+
+      App.init();
+      
+    },
+  },
+};
 </script>
 
 <style scoped>
-   h1{
-      text-align: center;
-      color:black;
-      margin: 30px;
-   }
+h1 {
+  text-align: center;
+  color: black;
+  margin: 30px;
+}
 
-   #logo{
-      width: auto;
-      height: 50px;
-      margin: 10px;
-      position: absolute;
-      top: 8px;
-      left: 0px;
-      }
-   .topright{
-      position: absolute;
-      top: 30px;
-      right: 16px;
-      font-size: 18px;
-      }
-   .topleft{
-      position: absolute;
-      top: 30px;
-      left: 16px;
-      font-size: 18px;}
-      .centered {
-      width: auto;
-      position: fixed;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-   }
+#logo {
+  width: auto;
+  height: 50px;
+  margin: 10px;
+  position: absolute;
+  top: 8px;
+  left: 0px;
+}
+.topright {
+  position: absolute;
+  top: 30px;
+  right: 16px;
+  font-size: 18px;
+}
+.topleft {
+  position: absolute;
+  top: 30px;
+  left: 16px;
+  font-size: 18px;
+}
+.centered {
+  width: auto;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 
-   #logbtn
-   {
-      width:300px;
-      height: 50px;
-   }
-   #vdivider
-   {
-      width:50px;
-   }
-   .test
-   {
-      text-align: center;
-      margin-bottom: 5px;
-      margin-top: 32px;
-   }
-   #SocialMediabtn
-   {
-      width:300px;
-      height: 50px;
-      margin: 10px;
-   }
-   hr.solid {
-      border-top: 3px solid #bbb;
-      margin:10px;
-   }
-
+#logbtn {
+  width: 300px;
+  height: 50px;
+}
+#vdivider {
+  width: 50px;
+}
+.test {
+  text-align: center;
+  margin-bottom: 5px;
+  margin-top: 32px;
+}
+#SocialMediabtn {
+  width: 300px;
+  height: 50px;
+  margin: 10px;
+}
+hr.solid {
+  border-top: 3px solid #bbb;
+  margin: 10px;
+}
 </style>
